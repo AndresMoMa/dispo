07e45dc110132e3cd626b6d8b79eebbcd03a8d9e
Andres Mora <andres_mm99@hotmail.com>
2024-09-06 13:48:12 -0500

    API

--- src/app/contaiD.tsx
+++ src/app/contaiD.tsx
@@ -19,7 +19,7 @@ const CardList: React.FC<CardProps> = (name,num) => {
         
         if (name.num=="0"){
           console.log("a")
-          const url="https://retoolapi.dev/Q9LB8S/dispo?ClientesI="+name.name
+          const url="https://retoolapi.dev/Cwgtgn/dispo?ClientesI="+name.name
         const response = await fetch(url);
         console.log(name)
         const result = await response.json();
@@ -32,7 +32,7 @@ const CardList: React.FC<CardProps> = (name,num) => {
         }
         if (name.num=="2"){
           console.log("arrre")
-          const url="https://retoolapi.dev/Q9LB8S/dispo?ClientesI=Gryffindor%20(SO%20Y%20APP)"
+          const url="https://retoolapi.dev/Cwgtgn/dispo?ClientesI=Gryffindor%20(SO%20Y%20APP)"
         const response = await fetch(url);
         console.log(name)
         const result = await response.json();
@@ -42,7 +42,7 @@ const CardList: React.FC<CardProps> = (name,num) => {
         }
         if (name.num=="1"){
           console.log("arrre")
-          const url="https://retoolapi.dev/Q9LB8S/dispo?ClientesI=Gryffindor (BD)"
+          const url="https://retoolapi.dev/Cwgtgn/dispo?ClientesI=Gryffindor (BD)"
         const response = await fetch(url);
         console.log(name)
         const result = await response.json();
