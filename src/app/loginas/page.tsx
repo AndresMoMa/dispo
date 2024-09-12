5e160aeba83883bb5e6285a3d1be3ef990315315
Andres Mora <andres_mm99@hotmail.com>
2024-09-12 01:55:07 +0000

    login v1

---
+++ src/app/loginas/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+    if (password=="Operador20249")
+      {
+        console.log("nose")
+        router.push('/')
+      }
+      else
+      {
+        alert("Contraseña incorrecta")
+      }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
+        <h2>Login</h2>
+  
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
---
+++ src/app/pages/ruta.tsx
@@ -0,0 +1,3 @@
+export default function ruta() {
+    return <div>About</div>
+  }
